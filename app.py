cf25c4ea42c017f9c4d31ef24bbc4db1ecaba3d0
ChiehYuChao <chiehyuc@student.unimelb.edu.au>
2024-05-07 20:41:00 +0800

    added css file

--- app.py
+++ app.py
@@ -128,4 +128,5 @@ def check_blood_pressure_result():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = '8080', debug=True)
+    # app.run(host='0.0.0.0', port = '8080', debug=True)
+    app.run(debug=True)
